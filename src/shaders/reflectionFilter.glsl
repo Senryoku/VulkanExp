b6fe020d4f0501f3546402052eb134cf232af4cf
Senryoku <maretverdant@gmail.com>
2022-01-18 13:35:29 +0100

    Edge-preserving filter for reflections. Revert to sharp reflections only for now as roughness guided ones are too noisy and distracting without 1/ an absurd amount of samples or 2/ way more sophisticated denoising.

--- src/shaders/reflectionFilter.glsl
+++ src/shaders/reflectionFilter.glsl
@@ -6,7 +6,13 @@ layout(set = 0, binding = 2, rgba32f) uniform image2D outImage;
 
 //#define DISABLE
 
-// TODO: Also use a depth-aware kernel? (See directLightFilter)
+#define EDGE_PRESERVING
+
+float gaussian(float stdDev, float dist) {
+    return (1 / (sqrt(2 * 3.14159) * stdDev)) * exp(-(dist * dist) / (2 * stdDev * stdDev));
+}
+
+const int maxDev = 32;
 
 void main()
 {
@@ -14,9 +20,11 @@ void main()
     vec4 final = vec4(0);
     float depth = imageLoad(positionDepth, coords).w;
     float roughness = imageLoad(inImage, coords).w;
-    float stdDev = max(1, 7 * roughness / (depth / 1.0)); // FIXME: This is arbitrary.
+    float stdDev = max(1, maxDev * roughness / max(1, (depth / 2.0))); // FIXME: This is arbitrary.
+    float depthStdDev = 1.0;                        // FIXME: Also arbitrary.
     float sqrDev = stdDev * stdDev;
-    int window = int(clamp(ceil(sqrt(-2 * sqrDev * log(0.01 * stdDev * sqrt(2 * 3.14159)))), 1, 7));
+    int window = int(clamp(ceil(sqrt(-2 * sqrDev * log(0.01 * stdDev * sqrt(2 * 3.14159)))), 1, maxDev));
+    float totalFactor = 0;
     // TODO: Base the kernel on the roughness. A better approximation should use as much available geometric data as possible (sample direction, depth...)
     for(int i = -window; i <= window; ++i) {
 #ifdef DIRECTION_X
@@ -24,8 +32,19 @@ void main()
 #else
         ivec2 offset = ivec2(0, i);
 #endif
-        final += (1 / sqrt(2 * 3.14159 * sqrDev)) * exp(-(i * i) / (2 * sqrDev)) * imageLoad(inImage, coords + offset);
+#ifdef EDGE_PRESERVING
+        float factor = gaussian(stdDev, i) * gaussian(depthStdDev, abs(depth - imageLoad(positionDepth, coords + offset).w));
+        totalFactor += factor;
+#else
+        float factor = gaussian(stdDev, i);
+#endif
+        final += factor * imageLoad(inImage, coords + offset);
     }
+#ifdef EDGE_PRESERVING
+    if(totalFactor > 1e-2)
+        final /= totalFactor;
+    else final = vec4(0);
+#endif
 #ifdef DISABLE
     imageStore(outImage, coords, imageLoad(inImage, coords));
 #else
